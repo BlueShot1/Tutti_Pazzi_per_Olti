6a7dc029eb7c05e073cdb9fe920ccdb8c2c03f69
BlueShot1 <73930392+BlueShot1@users.noreply.github.com>
2023-04-25 13:20:54 +0200

    aggiornamento grafico gg wp ancora uno schifo
    davide ha la patong migliore di olti

--- codice.js
+++ codice.js
@@ -1,14 +1,26 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
+const prezzoBenzina = 2;
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
+    var km = document.getElementById("km").value;
+    var kmXl = document.getElementById("kmXl").value;
+    var prezzoIniziale = document.getElementById("initPrice").value;
+    var anni = document.getElementById("year").value;
+  /*var data = google.visualization.arrayToDataTable([
     ['Year', 'oltigay', 'Expenses'],
     ['2004',  1000,      400],
     ['2005',  1170,      460],
     ['2006',  660,       1120],
-    ['2007',  1030,      540]
-  ]);
+    ['2007',  1030,      540]  
+    ]);*/
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'anni');
+    data.addColumn('number','spesa');
+    data.addRows([
+        ['0',Number(prezzoIniziale)],
+        [anni,Number(prezzoIniziale)+ Number((Number(km) * Number(anni))/Number(kmXl)*prezzoBenzina)]
+    ]);
 
   var options = {
     title: 'Company Performance',
